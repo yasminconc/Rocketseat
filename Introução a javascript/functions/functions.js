bce4b5be6f8399ee67f0530ec75452b2ca4a0056
Yasmin Silva da Conceição <yasmisilvaconc@gmail.com>
2025-03-18 17:08:12 -0300

    .

---
+++ Introução a javascript/functions/functions.js
@@ -0,0 +1,9 @@
+
+
+function createFrases () {
+    console.log("Estudar é muito ruim");
+    console.log("Paciência e persistência");
+    console.log("Revisão é a mãe do aprendizado");
+}
+
+createFrases()
---
+++ Introução a javascript/functions/functions1.js
@@ -0,0 +1,11 @@
+
+//function expression
+//function anonymous
+
+//parametros
+const sum = function (number1, number2) {
+    console.log(number1+ number2);
+    
+}
+
+sum(2, 3) // argumentos
