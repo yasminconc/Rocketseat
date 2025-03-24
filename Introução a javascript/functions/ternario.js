2c634e38d2f611da72bdf513dccf42297847f20e
Yasmin Silva da Conceição <yasmisilvaconc@gmail.com>
2025-03-24 17:01:43 -0300

    .

---
+++ Introução a javascript/functions/ternario.js
@@ -0,0 +1,18 @@
+//Condição então valor 1 se não valor 2
+//condition ? value1 : value2
+
+//Exemplos
+//café da manhã top
+
+let pao = true
+let queijo = true
+
+const niceBreakfast = pao && queijo ? 'Café show' : 'Café ruim'
+
+
+//Maior de 18
+
+
+let idade = 17
+
+const maiorDeIdade = idade >= 18 ? 'Você é maior de idade' : 'Você é menor de idade'
