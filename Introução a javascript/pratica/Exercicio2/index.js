0e52183c9918a9da4f22f1a7caa2ecdf47550ecb
Yasmin Silva da Conceição <yasmisilvaconc@gmail.com>
2025-04-07 17:40:36 -0300

    .

---
+++ Introução a javascript/pratica/Exercicio2/index.js
@@ -0,0 +1,12 @@
+/*
+    Solicite 2 números, faça a soma deles 
+    e apresente o resultado final ao usuário
+
+*/
+
+const numberOne = Number(prompt('Digite um número'))
+const numberTwo = Number(prompt('Digite outro número'))
+
+const soma = numberOne + numberTwo
+
+alert(`A soma dos números é ${soma}`)
